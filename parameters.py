d0d29addb46c393c7d7981cb37a7a46a8ebe86f4
Sebastian Bernasek <sbernasek@gmail.com>
2016-02-29 12:00:49 -0600

    firt commit

---
+++ parameters.py
@@ -0,0 +1,79 @@
+__author__ = 'Sebi'
+
+
+# species parameters:
+mrna_length = 1500  # nucleotides, ~1000 for e.coli, 1500 for yeast, 3k for drosophila
+protein_length = 400  # amino acids, 300 for e.coli, 385 for yeast, ~600 for mammals
+n_introns = 4  # roughly 2-8 for eukaryotes, maybe 2 for e.coli and 8 for humans
+
+# no clue
+PTR_RATE_CONSTANT = 0.1
+CATALYTIC_DEGRADATION_RATE_CONSTANT = 0.1
+MODIFICATION_RATE_CONSTANT = 0.1
+
+# iffy
+basal_transcription_rate = 0.1
+dna_binding_affinity = 1e2
+hill_coefficient = 1
+
+# settled rates
+promoter_strength = 30 * 60 / mrna_length  # mrna/min, based on 30 nt/s
+translation_constant = 10 * 60 / protein_length  # proteins/min, based on 10 amino acids per second
+mrna_decay_constant = 0.05
+protein_decay_constant = 0.025
+
+
+atp_requirements = {
+
+    'prokaryote': {
+
+        # initiation, elongation, termination
+        'transcription': [1, 2*mrna_length, 100],
+
+        # initiation, elongation, termination
+        'translation': [3, 4*protein_length, 1],
+
+        # activation/repression (could add translocation cost here)
+        'trancriptional_regulation': [5],
+
+        # energy required is re-used for elongation
+        'mrna_decay': [0],
+
+        # degradation (range ~0.25-1)
+        'protein_decay': [1*protein_length],
+
+        # phosphorylation
+        'modification': [1],
+
+        # miRNA silencing (could address dicer/ago synthesis, as well as decreasing miRNA synthesis cost)
+        'miRNA_silencing': [0],
+
+        # assumed similar to protein degradation... I have no idea
+        'catalytic_degradation': [1]},
+
+
+    'eukaryote': {
+        # initiation, elongation, termination, nucleosome barrier, splicing
+        'transcription': [50, 2*mrna_length, 900, 0.17*mrna_length, 10*n_introns],
+
+        # initiation, elongation, termination
+        'translation': [10, 4*protein_length, 1],
+
+        # activation/repression (could add translocation cost here)
+        'trancriptional_regulation': [10],
+
+        # energy required is re-used for elongation
+        'mrna_decay': [0],
+
+        # degradation (range ~0.25-1)
+        'protein_decay': [1*protein_length],
+
+        # phosphorylation
+        'modification': [1],
+
+        # miRNA silencing (could address dicer/ago synthesis, as well as decreasing miRNA synthesis cost)
+        'miRNA_silencing': [0],
+
+        # assumed similar to degradation... I have no idea
+        'catalytic_degradation': [1]}
+}
---
+++ reactions.py
@@ -0,0 +1,104 @@
+__author__ = 'Sebi'
+
+import functools
+from parameters import *
+
+
+class Reaction:
+
+    def __init__(self, reactants=None, products=None, rate_constant=1, consumed=None, rxn_type=None, atp_usage=None, cell_type='prokaryote'):
+        """
+        Class describes a single kinetic pathway.
+
+        Parameters:
+            reactants (list) - list of reactant indices
+            products (list) - list of product indices
+            rate_constant (float) - mass-action reaction rate constant
+            consumed (list) - lists which reactants are consumed by reaction
+            rxn_type (str) - identifies reaction type
+            atp_usage (float) - energetic cost of reaction in activated phosphorous equivalents (e.g. ~P), if none use defaults for specified rxn_type
+            cell_type (str) - 'prokaryote' or 'eukaryote', changes rate library
+        """
+        self.reactants = reactants
+        if reactants is None:
+            self.reactants = []
+
+        self.products = products
+        if products is None:
+            self.products = []
+
+        self.consumed = consumed
+        if consumed is None:
+            self.consumed = []
+
+        self.rate_constant = rate_constant
+        self.rxn_type = rxn_type
+
+        self.atp_usage = atp_usage
+        if self.atp_usage is None:
+            self.atp_usage = sum(atp_requirements[cell_type][rxn_type])
+
+    def get_rate(self, concentrations):
+        """
+        Compute and return current rate of a given pathway.
+
+        Parameters:
+            concentrations (list) - list of reactant species concentrations
+        Returns:
+            rate (float) - rate of reaction
+        """
+
+        activity = 1
+        if len(self.reactants) > 0:
+            activity = functools.reduce(lambda x, y: x*y, concentrations)
+        rate = self.rate_constant * activity
+        return rate
+
+
+class TranscriptionModification:
+
+    def __init__(self, substrate, target, mod_type, dissociation_constant, promotion_strength=1, cooperativity=1):
+        """
+        Class defines reactions that modify the rates of other reactions.
+
+        Parameters:
+            substrate (int) - index of transcription factor substrate
+            target (int) - index of mRNA whose transcription is repressed
+            mod_type (str) - indicates whether activation or repression kinetics are used
+            dissociation_constant (float) - gene binding affinity
+            rate_increase (float) - factor by which a promoter increases rate
+            cooperativity (float) - hill coefficient for transcription factor binding
+        """
+
+        self.substrate = substrate
+        self.target = target
+        self.dissociation_constant = dissociation_constant
+        self.promotion_strength = promotion_strength
+        self.cooperativity = cooperativity
+
+        if mod_type not in ['activation', 'repression']:
+            print('Error: Transcription factor type not recognized.')
+        else:
+            self.mod_type = mod_type
+
+    def get_rate_modifier(self, tf):
+        """
+        Returns transcription rate modification.
+
+        Parameters:
+            tf (float) - current level of transcription factor
+        Returns:
+            modifier (float) - current occupancy for transcription promotion or vacancy for repression
+        """
+
+        # get dissociation constant (dna binding affinity) and cooperativity (hill coefficient)
+        kd = self.dissociation_constant
+        n = self.cooperativity
+
+        if self.mod_type == 'activation':
+            sites_occupied = (tf**n) / ((kd**n) + (tf**n))
+            return self.promotion_strength * sites_occupied
+
+        elif self.mod_type == 'repression':
+            sites_unoccupied = (kd**n) / ((kd**n) + (tf**n))
+            return sites_unoccupied
