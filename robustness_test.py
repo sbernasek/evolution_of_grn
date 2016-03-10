752b72cced25e90ca24d5ebdc4dd3c3fe44a06da
Eric Johnson <EricJohnson@Erics-MacBook-Pro.local>
2016-03-10 10:59:13 -0600

    Ran adaptation test

--- robustness_test.py
+++ robustness_test.py
@@ -1,16 +1,17 @@
 from main import *
 import pickle
 
-generations = 5
-population_size = 5
+generations = 80
+population_size = 40
 num_mutants = 5  # it's a default argument in fitness.py, not used here
 
-print(generations*population_size*num_mutants*1.2/3600, ' hours')
+timetorun = generations*population_size*num_mutants*1.2/3600
+print('Program will take ~%4.2f hours to run!' % timetorun)
 
 # run simulation
 populations = run_simulation(generations=generations, population_size=population_size, mutations_per_division=2, test=robustness_test)
 
 # write results to serialized file
 filename = ('adaptation_test_results_Ngens_%d_Ncells_%d.p' % (generations, population_size))
-with open('robustness_test_results.p', 'wb') as f:
+with open(filename, 'wb') as f:
     pickle.dump(populations, f)
